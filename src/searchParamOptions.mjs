a1ef01834720d57260451012218a5df848f75a62
jujoco <jujocotm@gmail.com>
2024-03-03 02:25:18 -0800

    modify themeOptions

--- src/searchParamOptions.mjs
+++ src/searchParamOptions.mjs
@@ -2,17 +2,19 @@ const url = document.location.search;
 const searchParams = new URLSearchParams(url);
 const defaultMinutes = 50;
 const defaultBreak = 10;
-const defaultSets = 3;
-const defaultTheme = "ALL";
-const themeOptions = {
-	RED: "RED",
-	ORANGE: "ORANGE",
-	YELLOW: "YELLOW",
-	GREEN: "GREEN",
-	TEAL: "TEAL",
-	BLUE: "BLUE",
-	PURPLE: "PURPLE",
-	MAGENTA: "MAGENTA",
+const defaultSessions = 3;
+export const themeOptions = {
+	OFF: "themeOFF",
+	ALL: "themeALL",
+	WHITE: "themeWHITE",
+	RED: "themeRED",
+	ORANGE: "themeORANGE",
+	YELLOW: "themeYELLOW",
+	GREEN: "themeGREEN",
+	TEAL: "themeTEAL",
+	BLUE: "themeBLUE",
+	PURPLE: "themePURPLE",
+	MAGENTA: "themeMAGENTA",
 };
 
 /** @type {number} paramMinutes */
@@ -21,15 +23,16 @@ let paramMinutes = parseInt(searchParams.get("focus")) || defaultMinutes;
 /** @type {number} paramBreak */
 let paramBreak = parseInt(searchParams.get("break")) || defaultBreak;
 
-/** @type {number} paramSets */
-let paramSets = parseInt(searchParams.get("sets")) || defaultSets;
+/** @type {number} paramSessions */
+let paramSessions = parseInt(searchParams.get("sessions")) || defaultSessions;
 
 const parseTheme = searchParams.get("theme")?.toUpperCase();
 /** @type {string} paramTheme */
-const paramTheme = themeOptions[parseTheme] || defaultTheme;
+const paramTheme = themeOptions[parseTheme] || themeOptions.OFF;
 
 paramMinutes = paramMinutes < 1 || paramMinutes > 60 ? 50 : paramMinutes;
 paramBreak = paramBreak < 1 || paramBreak > 60 ? defaultBreak : paramBreak;
-paramSets = paramSets < 1 || paramSets > 10 ? defaultSets : paramSets;
+paramSessions =
+	paramSessions < 1 || paramSessions > 10 ? defaultSessions : paramSessions;
 
-export { paramMinutes, paramBreak, paramSets, paramTheme };
+export { paramMinutes, paramBreak, paramSessions, paramTheme };
